dd6134a7b6c58a6950fd31bd172dc32c5246b137
xovest <79008917+xovest@users.noreply.github.com>
2021-12-01 08:27:20 +0200

    modif

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Articles = () => {
+const User = () => {
+  const [profile, setProfile] = useState(null);
+
+  // runs automatically after initial render
+  useEffect(() => {
+    setTimeout( async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+      const data = await res.json();
+      setProfile(data);
+    }, 5000)
+  }, [])
+
   return (
-    <div className="articles">
-      <h2>All Articles</h2>
+    <div className="user">
+      <h2>User Details</h2>
+
+      {profile && (
+        <div className="profile">
+          <h3>{ profile.username }</h3>
+          <p>{ profile.email }</p>
+          <a href={ profile.website }>{ profile.website }</a>
+        </div>
+      )}
+
+      {!profile && <div>Loading...</div>}
     </div>
   )
 }
 
-export default Articles
+export default User
